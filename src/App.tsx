1a0001169be2c8ee8768fe8f4b0e79cbd57a3a80
Anurag Deore <anurag.deore@clootrack.com>
2024-07-09 23:59:02 +0530

    add flex

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import {  Flex, Heading, Text } from "@chakra-ui/react";
 import UserPage from "./features/users/Users";
 
 function App() {
   return (
     <>
-      <Box p="5" shadow={"md"}>
+      <Flex align={"center"} gap={3} p="5" shadow={"md"}>
         <Heading size="md">Neo App</Heading>
         <Text fontSize={"medium"}>A POC of Neo APP</Text>
-      </Box>
+      </Flex>
       <UserPage />
     </>
   );
